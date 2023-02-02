33945488e1962b441b80e4c0bccb788f4e24ff2a
Alexei Rusu <rusualexei1995@gmail.com>
2023-02-01 21:11:45 -0500

    landscaper: user can buy scissors

--- landscaper.py
+++ landscaper.py
@@ -1,11 +1,10 @@
 ############ LANDSCAPER Game ##############
 
 
-
-
 game = {
     "tool": 0,
     "money": 0
+    # "tired": 0
 }
 
 tools = [
@@ -18,18 +17,34 @@ def mow_lawn():
     print(f"Mowing the lawn with {tool['name']} for {tool['profit']}")
     game["money"] += tool["profit"]
     
+def upgrade():
+    next_tool = tools[game["tool"] + 1] 
+    # print(next_tool)
+    if(game["money"] < next_tool["price"]):
+        print("Not enouth money for buying this tool")
+        return 0 
+    if(next_tool == None):
+        print("There are no more tools available")
+    game["money"] -= next_tool["price"]
+    game["tool"] += 1
+    print(f"You upgrated to {next_tool['name']}")
+
+    
 def check_stats():
     print(f"You have {game['money']} $ ")
 
 
 while(True):
-    user_choice = input("[1] Mow Lawn [2] Check stats [Q] Quit Game")
+    user_choice = input("[1] Mow Lawn [2] Check stats [3] Upgrade [Q] Quit Game")
     
     if(user_choice == "1"):
         mow_lawn()
         
     if(user_choice == "2"):
         check_stats()
+        
+    if(user_choice == "3"):
+        upgrade()
     
     if(user_choice == "Q"):
         break
